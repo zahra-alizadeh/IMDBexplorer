3e81c0f289af11d4ce2c8009b1bd252d2b357bee
zahra alizadeh <zahraalizade250@gmail.com>
2020-06-27 18:29:25 +0430

    update DB

--- application/model/CreateDB.php
+++ application/model/CreateDB.php
@@ -7,72 +7,104 @@ require_once ('Model.php');
 class CreateDB extends Model
 {
     private $createTableQueries = array(
-        "CREATE TABLE `users` (
-          `id` int(11) NOT NULL AUTO_INCREMENT,
-          `username` varchar(100) COLLATE utf8_persian_ci NOT NULL,
-          `email` varchar(100) COLLATE utf8_persian_ci NOT NULL,
-          `password` varchar(100) COLLATE utf8_persian_ci NOT NULL,
-          `admin` enum('user','admin') COLLATE utf8_persian_ci NOT NULL DEFAULT 'user',
-          `created_at` datetime NOT NULL,
-          `updated_at` datetime DEFAULT NULL,
-          PRIMARY KEY (`id`),
-          UNIQUE KEY `email` (`email`),`username` (`username`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
+//        "CREATE TABLE `users` (
+//          `id` int(11) NOT NULL AUTO_INCREMENT,
+//          `username` varchar(100) COLLATE utf8_persian_ci NOT NULL,
+//          `email` varchar(100) COLLATE utf8_persian_ci NOT NULL,
+//          `password` varchar(100) COLLATE utf8_persian_ci NOT NULL,
+//          `admin` enum('user','admin') COLLATE utf8_persian_ci NOT NULL DEFAULT 'user',
+//          `created_at` datetime NOT NULL,
+//          `updated_at` datetime DEFAULT NULL,
+//          PRIMARY KEY (`id`),
+//          UNIQUE KEY `email` (`email`),`username` (`username`)
+//        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
+//
+//        "CREATE TABLE `actors` (
+//          `id` int(11) NOT NULL AUTO_INCREMENT,
+//          `first_name` varchar(100) COLLATE utf8_persian_ci NOT NULL,
+//          `last_name` varchar(100) COLLATE utf8_persian_ci NOT NULL,
+//          `birth_date` datetime ,
+//          `birth_place` varchar(100) ,
+//          `bio` varchar(200) COLLATE utf8_persian_ci,
+//          `created_at` datetime NOT NULL,
+//          `updated_at` datetime DEFAULT NULL,
+//          PRIMARY KEY (`id`),
+//        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
 
-        "CREATE TABLE `movies` (
-          `id` int(11) NOT NULL AUTO_INCREMENT,
-          `movie_name` varchar(100) COLLATE utf8_persian_ci NOT NULL,
-          `release_date` datetime,
-          `trailer` varchar(100) COLLATE utf8_persian_ci,
+//        "CREATE TABLE `actor_pictures` (
+//          `actor_id` int(11) NOT NULL ,
+//          `picture` varchar(200) COLLATE utf8_persian_ci,
+//          `created_at` datetime NOT NULL,
+//          `updated_at` datetime DEFAULT NULL,
+//          PRIMARY KEY (`actor_id`,`picture`),
+//          FOREIGN KEY (`actor_id`) REFERENCES `actors` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+//        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
+
+
+//        "CREATE TABLE `directors` (
+//          `id` int(11) NOT NULL AUTO_INCREMENT,
+//          `first_name` varchar(100) COLLATE utf8_persian_ci NOT NULL,
+//          `last_name` varchar(100) COLLATE utf8_persian_ci NOT NULL,
+//          `birth_date` datetime ,
+//          `birth_place` varchar(100) ,
+//          `picture` varchar(200) COLLATE utf8_persian_ci,
+//          `bio` varchar(200) COLLATE utf8_persian_ci,
+//          `created_at` datetime NOT NULL,
+//          `updated_at` datetime DEFAULT NULL,
+//          PRIMARY KEY (`id`),
+//        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
+
+        "CREATE TABLE `director_pictures` (
+          `director_id` int(11) NOT NULL ,
           `picture` varchar(200) COLLATE utf8_persian_ci,
-          `score` varchar(200) COLLATE utf8_persian_ci,
-          `summary` varchar(200) COLLATE utf8_persian_ci,
           `created_at` datetime NOT NULL,
           `updated_at` datetime DEFAULT NULL,
-          PRIMARY KEY (`id`)
+          PRIMARY KEY (`director_id`,`picture`),
+          FOREIGN KEY (`director_id`) REFERENCES `directors` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
 
-        "CREATE TABLE `movie_genre` (
-          `movie_id` int(11) NOT NULL,
-          `genre` varchar(100) COLLATE utf8_persian_ci NOT NULL,
-          `created_at` datetime NOT NULL,
-          `updated_at` datetime DEFAULT NULL,
-          PRIMARY KEY (`movie_id`,`genre`),
-          FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
-
-        "CREATE TABLE `watchList` (
-          `movie_id` int(11) NOT NULL,
-          `user_id` int(11) NOT NULL,
-          `created_at` datetime NOT NULL,
-          `updated_at` datetime DEFAULT NULL,
-          PRIMARY KEY (`movie_id`,`user_id`),
-          FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-          FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
-
-        "CREATE TABLE `persons` (
-          `id` int(11) NOT NULL AUTO_INCREMENT,
-          `first_name` varchar(100) COLLATE utf8_persian_ci NOT NULL,
-          `last_name` varchar(100) COLLATE utf8_persian_ci NOT NULL,
-          `birth_date` datetime ,
-          `birth_place` varchar(100) ,
-          `picture` varchar(200) COLLATE utf8_persian_ci,
-          `bio` varchar(200) COLLATE utf8_persian_ci,
-          `role` enum('director','actor&director','both') COLLATE utf8_persian_ci NOT NULL,
-          `created_at` datetime NOT NULL,
-          `updated_at` datetime DEFAULT NULL,
-          PRIMARY KEY (`id`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
-
-        "CREATE TABLE `movie_director` (
-          `movie_id` int(11) NOT NULL,
-          `director_id` int(11) NOT NULL,
-          `created_at` datetime NOT NULL,
-          `updated_at` datetime DEFAULT NULL,
-          PRIMARY KEY (`movie_id`,`director_id`),
-          FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
+//        "CREATE TABLE `movies` (
+//          `id` int(11) NOT NULL AUTO_INCREMENT,
+//          `movie_name` varchar(100) COLLATE utf8_persian_ci NOT NULL,
+//          `director` int(11) NOT NULL,
+//          `release_date` datetime,
+//          `trailer` varchar(100) COLLATE utf8_persian_ci,
+//          `picture` varchar(200) COLLATE utf8_persian_ci,
+//          `score` varchar(200) COLLATE utf8_persian_ci,
+//          `summary` varchar(200) COLLATE utf8_persian_ci,
+//          `created_at` datetime NOT NULL,
+//          `updated_at` datetime DEFAULT NULL,
+//          PRIMARY KEY (`id`)
+//        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
+//
+//        "CREATE TABLE `movie_genre` (
+//          `movie_id` int(11) NOT NULL,
+//          `genre` varchar(100) COLLATE utf8_persian_ci NOT NULL,
+//          `created_at` datetime NOT NULL,
+//          `updated_at` datetime DEFAULT NULL,
+//          PRIMARY KEY (`movie_id`,`genre`),
+//          FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+//        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
+//
+//        "CREATE TABLE `watchList` (
+//          `movie_id` int(11) NOT NULL,
+//          `user_id` int(11) NOT NULL,
+//          `created_at` datetime NOT NULL,
+//          `updated_at` datetime DEFAULT NULL,
+//          PRIMARY KEY (`movie_id`,`user_id`),
+//          FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+//          FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+//        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
+//
+//
+//        "CREATE TABLE `movie_director` (
+//          `movie_id` int(11) NOT NULL,
+//          `director_id` int(11) NOT NULL,
+//          `created_at` datetime NOT NULL,
+//          `updated_at` datetime DEFAULT NULL,
+//          PRIMARY KEY (`movie_id`,`director_id`),
+//          FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+//        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
 
         "CREATE TABLE `movie_actor` (
           `movie_id` int(11) NOT NULL,
@@ -80,35 +112,35 @@ class CreateDB extends Model
           `created_at` datetime NOT NULL,
           `updated_at` datetime DEFAULT NULL,
           PRIMARY KEY (`movie_id`,`actor_id`),
-          FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+          FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
 
 
-        "CREATE TABLE `comments` (
-          `id` int(11) NOT NULL AUTO_INCREMENT,
-          `user_id` int(11) NOT NULL,
-          `movie_id` int(11)  NOT NULL,
-          `text` text COLLATE utf8_persian_ci NOT NULL,
-          `permission` enum('unseen','seen','approved') COLLATE utf8_persian_ci NOT NULL DEFAULT 'unseen',
-          `created_at` datetime NOT NULL,
-          `updated_at` datetime DEFAULT NULL,
-          PRIMARY KEY (`id`),
-          FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-          FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE)
-           ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
-
-        "CREATE TABLE `blogs` (
-          `id` int(11) NOT NULL AUTO_INCREMENT,
-          `movie_id` int(11)  NOT NULL,
-          `person_id` int(11)  NOT NULL,
-          `picture` varchar(200) COLLATE utf8_persian_ci,
-          `text` text COLLATE utf8_persian_ci NOT NULL,
-          `created_at` datetime NOT NULL,
-          `updated_at` datetime DEFAULT NULL,
-          PRIMARY KEY (`id`),
-          FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-          FOREIGN KEY (`person_id`) REFERENCES `persons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE)
-           ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
+//        "CREATE TABLE `comments` (
+//          `id` int(11) NOT NULL AUTO_INCREMENT,
+//          `user_id` int(11) NOT NULL,
+//          `movie_id` int(11)  NOT NULL,
+//          `text` text COLLATE utf8_persian_ci NOT NULL,
+//          `permission` enum('unseen','seen','approved') COLLATE utf8_persian_ci NOT NULL DEFAULT 'unseen',
+//          `created_at` datetime NOT NULL,
+//          `updated_at` datetime DEFAULT NULL,
+//          PRIMARY KEY (`id`),
+//          FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+//          FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE)
+//           ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
+//
+//        "CREATE TABLE `blogs` (
+//          `id` int(11) NOT NULL AUTO_INCREMENT,
+//          `movie_id` int(11)  NOT NULL,
+//          `person_id` int(11)  NOT NULL,
+//          `picture` varchar(200) COLLATE utf8_persian_ci,
+//          `text` text COLLATE utf8_persian_ci NOT NULL,
+//          `created_at` datetime NOT NULL,
+//          `updated_at` datetime DEFAULT NULL,
+//          PRIMARY KEY (`id`),
+//          FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+//          FOREIGN KEY (`person_id`) REFERENCES `persons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE)
+//           ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
 
 //        "CREATE TABLE `votes` (
 //          `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -141,9 +173,9 @@ class CreateDB extends Model
 
     public function run()
     {
-//        foreach ($this->createTableQueries as $createTableQuery) {
-//            $this->createTable($createTableQuery);
-//        }
+        foreach ($this->createTableQueries as $createTableQuery) {
+            $this->createTable($createTableQuery);
+        }
         foreach ($this->tableInitializes as $tableInitialize) {
             $this->insert($tableInitialize['table'], $tableInitialize['fields'], $tableInitialize['values']);
         }
